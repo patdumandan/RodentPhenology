713ca1e585a44ed2253a438d7064bb038b107c1f
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2021-01-04 18:29:54 -0500

    add script for prelim plots

--- all_sp_betabinom.R
+++ all_sp_betabinom.R
@@ -1,19 +1,19 @@
 #DATA MANIPULATION####
-all_props$trt<-ifelse(all_props$trt=="control", 0, 1)
-all_props$years=(all_props$year-mean(all_props$year))/(2*sd(all_props$year))
-all_props[is.na(all_props)] <- 0 #set non-detects to 0
-all_props$spcode=as.integer(all_props$species)
+all_sp$trt<-ifelse(all_sp$trt=="control", 0, 1) 
+all_sp$years=(all_sp$year-mean(all_sp$year))/(2*sd(all_sp$year)) #standardize year
+all_sp[is.na(all_sp)] <- 0 #set non-detects to 0
+all_sp$spcode=as.integer(all_sp$species) 
 
 dat_list=list(
-  N=length(all_props$month),
-  y=all_props$repro,
-  n=all_props$count,
-  year=all_props$years,
-  treatment=all_props$trt,
-  species=all_props$spcode,
-  month=all_props$month, 
-  Nmon=length(unique(all_props$month)),
-  Nsp=length(unique(all_props$species))
+  N=length(all_sp$month),
+  y=all_sp$repro,
+  n=all_sp$count,
+  year=all_sp$years,
+  treatment=all_sp$trt,
+  species=all_sp$spcode,
+  month=all_sp$month, 
+  Nmon=length(unique(all_sp$month)),
+  Nsp=length(unique(all_sp$species))
 )
 
 all_prop_male_mod=stan(model_code = "
@@ -48,8 +48,8 @@ data{
   vector <lower=0> [N] B;
   vector [Nmon] alpha_mon; //random intercept per species
    vector [Nsp] alpha_sp; //random intercept per species
-  //vector [Nmon] yr_mon; //random slope per family for year effect
-  //vector [Nmon] trt_mon;//random slope per family for treatment effect
+  //vector [Nmon] yr_mon; //random slope per month for year effect
+  //vector [Nmon] trt_mon;//random slope per month for treatment effect
 
   
   for (j in 1:Nmon) {
@@ -103,5 +103,54 @@ data{
   }", data=dat_list, chains=4, iter=3000)
 
 #MODEL OUTPUT####
-print(all_prop_male_mod, pars=c("alpha","alpha_mon", "alpha_sp","trt_eff", "year_eff"))
+print(mod1, pars=c("alpha","alpha_mon", "alpha_sp","trt_eff", "year_eff"))
 saveRDS(all_prop_male_mod, "all_sp_betabinom_mod.RDS")
+mod1=readRDS("all_sp_betabinom_mod.RDS")
+
+#OUTPUT VISUALIZATION####
+plot_all_raw=all_sp%>%
+  ggplot(data=all_sp, mapping=aes(x=reorder(Month,month), y=repro, fill=trt))+
+  geom_col(position=position_dodge())+
+  xlab("Month")+ylab("No.of reproductive individuals")+
+  facet_wrap(~species)+theme(axis.text.x = element_text(angle = 45, hjust=1),
+  panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+  labs(fill="set-up")+
+  scale_fill_grey()
+plot_all_raw
+
+total_sp_grp=rbind(PB_props, PE_props, PP_props, PF_props, PM_props, RM_props)%>%
+      group_by(month,Month, species,trt)%>%
+      summarize(total=n())%>%
+  arrange(total)
+
+plot_all_grp=total_sp_grp%>%
+  ggplot(mapping=aes(x=reorder(Month,month), y=total, fill=trt))+
+  geom_col(position=position_dodge())+
+  scale_fill_grey()+
+  xlab("Month")+ylab("No.of reproductive individuals")+
+  theme(axis.text.x = element_text(hjust=1, angle=45), 
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+         panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+  labs(fill="set-up")+
+  facet_wrap(~species)
+plot_all_grp
+
+total_sp_yr=rbind(PB_props, PE_props, PP_props, PF_props, PM_props, RM_props)%>%
+  group_by(month,Month,year, trt)%>%
+  summarize(total=n())%>%
+  mutate(total_yr=sum(total))
+
+plot_all_year=total_sp_yr%>%
+  ggplot(mapping=aes(x=year, y=total, fill=trt))+
+  geom_col(position=position_dodge())+
+  xlab("Month")+ylab("No.of reproductive individuals")+
+  facet_wrap(~month)
+plot_all_year
+
+plot_all_year=all_sp%>%
+  ggplot(mapping=aes(x=year, y=repro, fill=trt))+
+  geom_col(position = position_dodge())+labs(fill="set-up")+
+  xlab("Year")+ylab("No.ofreproductive individuals")
+plot_all_year
+
