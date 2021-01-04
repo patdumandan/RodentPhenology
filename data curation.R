6778e4430d7171eef8a8bc3fe32ab3536f876b09
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2021-01-04 18:29:28 -0500

    recode month and add script to build female reproductive indivs dataset

--- data curation.R
+++ data curation.R
@@ -3,8 +3,12 @@ library(tidyr)
 
 Portal_rodent=read.csv("./PortalData/Rodents/Portal_rodent.csv")
 Portal_plots=read.csv("./PortalData/SiteandMethods/Portal_plots.csv")
-portal1=inner_join(Portal_rodent, Portal_plots, by= c("plot", "month", "year"))
+portal1=inner_join(Portal_rodent, Portal_plots, by= c("plot", "Month", "year"))%>%
+  mutate(Month=recode(Month, "1"= "Jan", "2"="Feb", "3"="Mar", "4"="Apr",
+                      "5"="May","6"="Jun", "7"="Jul", "8"= "Aug", "9"="Sept",
+                      "10"="Oct","11"="Nov", "12"="Dec"))
 
+#male individuals####
 #size thresholds####
 PB=portal1%>%
   filter(species=="PB",testes %in% target_repro,wgt>=16)%>%
@@ -31,31 +35,29 @@ RM=portal1%>%
   filter(species=="RM",testes %in% target_repro, wgt>=4)%>%
   arrange(wgt)
 
-
-
 #### PB proportions#####
 
 PBex=portal1%>%
   filter(species=="PB",testes %in% target_repro,wgt>=16, treatment=="exclosure")%>%
-  group_by(month, year)%>%
+  group_by(month,Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="exclosure")
 
 PBcon=portal1%>%
   filter(species=="PB",testes %in% target_repro,wgt>=16, treatment=="control")%>%
-  group_by(month, year)%>%
+  group_by(month,Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="control")
 
 PB_all_ex=portal1%>%
   filter(species=="PB", treatment=="exclosure", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(month,Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="exclosure")
 
 PB_all_con=portal1%>%
   filter(species=="PB", treatment=="control", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(month,Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="control")
 
@@ -73,25 +75,25 @@ PB_props=read.csv("PB_props.csv")
 
 PPex=portal1%>%
   filter(species=="PP",testes %in% target_repro,wgt>=10, treatment=="exclosure")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="exclosure")
 
 PPcon=portal1%>%
   filter(species=="PP",testes %in% target_repro,wgt>=10, treatment=="control")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="control")
 
 PP_all_ex=portal1%>%
   filter(species=="PP", treatment=="exclosure", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="exclosure")
 
 PP_all_con=portal1%>%
   filter(species=="PP", treatment=="control", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="control")
 
@@ -109,25 +111,25 @@ PP_props=read.csv("PP_props.csv")
 
 PEex=portal1%>%
   filter(species=="PE",testes %in% target_repro,wgt>=7, treatment=="exclosure")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="exclosure")
 
 PEcon=portal1%>%
   filter(species=="PE",testes %in% target_repro,wgt>=7, treatment=="control")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="control")
 
 PE_all_ex=portal1%>%
   filter(species=="PE", treatment=="exclosure", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="exclosure")
 
 PE_all_con=portal1%>%
   filter(species=="PE", treatment=="control", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="control")
 
@@ -144,25 +146,25 @@ PE_props=read.csv("PE_props.csv")
 
 PFex=portal1%>%
   filter(species=="PF",testes %in% target_repro,wgt>=4, treatment=="exclosure")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="exclosure")
 
 PFcon=portal1%>%
   filter(species=="PF",testes %in% target_repro,wgt>=4, treatment=="control")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="control")
 
 PF_all_ex=portal1%>%
   filter(species=="PF", treatment=="exclosure", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="exclosure")
 
 PF_all_con=portal1%>%
   filter(species=="PF", treatment=="control", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="control")
 
@@ -179,25 +181,25 @@ PF_props=read.csv("PF_props.csv")
 
 PMex=portal1%>%
   filter(species=="PM",testes %in% target_repro,wgt>=11, treatment=="exclosure")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="exclosure")
 
 PMcon=portal1%>%
   filter(species=="PM",testes %in% target_repro,wgt>=11, treatment=="control")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="control")
 
 PM_all_ex=portal1%>%
   filter(species=="PM", treatment=="exclosure", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="exclosure")
 
 PM_all_con=portal1%>%
   filter(species=="PM", treatment=="control", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="control")
 
@@ -214,25 +216,25 @@ PM_props=read.csv("PM_props.csv")
 
 RMex=portal1%>%
   filter(species=="RM",testes %in% target_repro,wgt>=4, treatment=="exclosure")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="exclosure")
 
 RMcon=portal1%>%
   filter(species=="RM",testes %in% target_repro,wgt>=4, treatment=="control")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(repro=n())%>%
   mutate(trt="control")
 
 RM_all_ex=portal1%>%
   filter(species=="RM", treatment=="exclosure", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="exclosure")
 
 RM_all_con=portal1%>%
   filter(species=="RM", treatment=="control", sex=="M")%>%
-  group_by(month, year)%>%
+  group_by(Month, year)%>%
   summarize(count=n())%>%
   mutate(trt="control")
 
@@ -251,5 +253,46 @@ all_sp=rbind(PB_props, PE_props, PP_props, PF_props, PM_props, RM_props)
 
 write.csv(all_sp, "all_props_NA.csv")
 all_props=read.csv("all_props_NA.csv")
-
+hist(all_props$proportion) #overdispersed 0 and 1 proportions
 max(all_sp$proportion)
+
+#female indivs####
+target_vag=c("S", "P", "B")
+target_nip=c("R", "E", "B")
+target_sp=c("PB", "PP", "PE", "PF", "PM", "RM")
+portal2=portal1%>%
+  filter(vagina %in% target_vag | nipples %in% target_nip |
+           pregnant== "P" | lactation=="L")
+
+portal3=portal2%>%
+  filter(species %in%target_sp)
+
+write.csv(portal3, "female_repro.csv")
+fem_rep=read.csv("female_repro.csv")
+
+
+PBf=fem_rep%>%
+  filter(species=="PB", wgt>=20)%>%
+  arrange(wgt)
+
+PPf=fem_rep%>%
+  filter(species=="PP", wgt>=5)%>%
+  arrange(wgt)
+#have an individual considered juvenile but with enlarged nipples
+
+PE=portal1%>%
+  filter(species=="PE",testes %in% target_repro, wgt >=7)%>%
+  arrange(wgt)
+
+PF=portal1%>%
+  filter(species=="PF",testes %in% target_repro, wgt>=4)%>%
+  arrange(wgt)
+
+PM=portal1%>%
+  filter(species=="PM",testes %in% target_repro, wgt >=11)%>%
+  arrange(wgt)
+#1 individual identified as juv but size within reproductive??
+
+RM=portal1%>%
+  filter(species=="RM",testes %in% target_repro, wgt>=4)%>%
+  arrange(wgt)
