96acf7dcad71bd760b4663a6165be24080734d01
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2021-01-18 22:17:51 -0500

    added script for patterns in monthly proportions per plot

--- RScripts/plots.R
+++ RScripts/plots.R
@@ -1,5 +1,5 @@
 all_fem_props=read.csv("./reconfigured_data/all_fem_props.csv")
-
+all_props=read.csv("./reconfigured_data/all_props_NA.csv")
 sum(all_fem_props$count)
 sum(all_fem_props$repro, na.rm=T) #5234/15568=0.34
 
@@ -10,11 +10,11 @@ sum(con1$count, na.rm=T)#31%
 
 require(ggplot2)
 require(ggplotgui)
-ggplot(all_props, aes(x = year, y = proportion, colour = Month)) +
-  geom_point()+
+ggplot(allmc, aes(x = year, y=proportion)) +
+  geom_point(alpha=0.2)+
   geom_smooth(se = FALSE, method = 'lm') +
-  #facet_grid( . ~ month ) +
-  theme_bw()+ggtitle("PP male")
+  #facet_grid( . ~ trt ) +
+  theme_bw()+ggtitle("male (control)")
 
 
 PEf=ggplot(PEf_props, aes(x = year, y = proportion, colour = trt)) +
@@ -55,4 +55,29 @@ PB_props%>%
   geom_density(position = "dodge")+
   facet_wrap(~month)
 
-qplot(y=proportion, data=PBf_props, geom="histogram")
+# plots per treatment####
+male_ex=all_sp%>%filter(!is.na(count), !is.na(repro))%>%group_by(Month,species, trt)%>%summarize(total_n=sum(count), total_rep=sum(repro), total_prop=total_rep/total_n)%>%select(Month, species, total_n, total_rep, total_prop, trt)%>%
+  mutate(month=recode(Month, "Jan"="1", "Feb"="2", "Mar"="3", "Apr"="4",
+     "May"="5","Jun"="6", "Jul"="7", "Aug"="8","Sept"= "9",
+      "Oct"="10","Nov"="11", "Dec"="12"))
+
+
+graph <- ggplot(male_ex, mapping=aes(x = reorder(Month,month), y = total_prop, colour = trt)) +
+  geom_boxplot(notch = FALSE) +
+theme_bw()
+ graph
+
+write.csv(male_ex, "monthly_male_trt.csv")
+
+
+female_ex=all_female_sp%>%filter(!is.na(count), !is.na(repro))%>%group_by(Month,species, trt)%>%summarize(total_n=sum(count), total_rep=sum(repro), total_prop=total_rep/total_n)%>%select(Month, species, total_n, total_rep, total_prop, trt)%>%
+  mutate(month=recode(Month, "Jan"="1", "Feb"="2", "Mar"="3", "Apr"="4",
+                      "May"="5","Jun"="6", "Jul"="7", "Aug"="8","Sept"= "9",
+                      "Oct"="10","Nov"="11", "Dec"="12"))
+
+write.csv(female_ex, "monthly_female_trt.csv")
+
+graph <- ggplot(female_ex, mapping=aes(x = reorder(Month,month), y = total_prop, colour = trt)) +
+  geom_boxplot(notch = FALSE) +
+  theme_bw()+ggtitle("Female")
+graph
