2642f0cb9388b41e6c2f3a16c9839bacf0f1cf07
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2021-05-18 09:24:48 -0400

    set df and knots as NULL

--- RScripts/splines_rodents_normal.R
+++ RScripts/splines_rodents_normal.R
@@ -12,8 +12,9 @@ PB_female_con=pb_plot%>%filter(treatment=="control", sex=="female")
 PB_female_ex=pb_plot%>%filter(treatment=="exclosure", sex=="female")
 
 X1 <- PB_female_con$month
-X2= PB_female_con$years
-B <- t(bs(X1, knots=seq(-5,5,1), degree=3, intercept = TRUE)) # creating the B-splines
+X2= PB_female_con$lag_ndvi
+X3= PB_female_con$lag_ppt
+B <- t(bs(X1, df=NULL, knots=NULL, degree=3, intercept = TRUE)) # creating the B-splines
 num_data <- length(X1)
 num_basis <- nrow(B)
 Y = PB_female_con$proportion
@@ -24,49 +25,38 @@ data {
   int num_basis; //no. of basis (order-1) 
   real <lower=0,upper=1>Y[num_data]; //response variable (e.g., no.of breeding obs.)
   vector[num_data] X1; //month
- // vector[num_data] X2; //ndvi 
+  vector[num_data] X2; //ndvi 
+ // vector[num_data] X3; //precip 
   matrix[num_basis, num_data] B; //matrix of coefficients of splines(rows), length of X1 (columns)
 } 
  
 parameters { 
   row_vector[num_basis] a_raw; // smooth terms for month
   real a0; //intercept
-//  real ndvi_eff;
+  real ndvi_eff;
   real<lower=0> sigma; //error term 
   real<lower=0> tau; // for noncentered parameterization of spline coefficients
 } 
  
 transformed parameters { 
   row_vector[num_basis] a; //noncentered parameters of splines
-  vector <lower=0, upper=1> [num_data] repro_mu; //estimated P(breeding) 
-  vector <lower=0> [num_data] a1;
-  vector <lower=0> [num_data] b1;
+  vector[num_data] Y_hat; 
+  
   a = a_raw*tau;  
 
- for (i in 1:num_data){
- 
- repro_mu=inv_logit(a0*X1 + to_vector(a*B));
+ Y_hat=a0*X1 + ndvi_eff*X2 + to_vector(a*B);
  }
- 
- a1=repro_mu*tau;
- b1=(1-a1)*tau;
-}
 
 model { 
   a_raw ~ normal(0, 1); 
-  tau ~ cauchy(0, 1); 
-  sigma ~ cauchy(0, 1); 
- // ndvi_eff~normal(0,1);
-
-
-  for (i in 1:num_data){
-  
-  Y[i]~ beta(a1, b1);
-  }
-
-}",
+  tau ~ normal(0, 1); 
+  sigma ~ normal(0, 1); 
+  ndvi_eff~normal(0,1);
+  Y~ normal(Y_hat, sigma);
+  }",
 iter=300, control=list(adapt_delta=0.95), 
 data =list(X1 =X1, # generating inputs
+           X2=X2,
            B =B, # creating the B-splines
            num_data=num_data,
            num_basis=num_basis,
@@ -80,13 +70,15 @@ Y_hat_lb <- array(NA, length(Y)) #lower boundary
 
 for (i in 1:length(Y)) {
   Y_hat_med[i] <- median(ff$Y_hat[,i]);
-  Y_hat_lb[i] <- quantile(ff$Y_hat[,i],probs = 0.25)
-  Y_hat_ub[i] <- quantile(ff$Y_hat[,i],probs = 0.75)
+  Y_hat_lb[i] <- quantile(ff$Y_hat[,i],probs = 0.025)
+  Y_hat_ub[i] <- quantile(ff$Y_hat[,i],probs = 0.975)
 }
 
 plot(X1,Y, xaxt="n") #plot raw data
 axis(1, c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"), at=c(1:12))
-lines(smooth.spline(X, Y_hat_med), col="blue")
+lines(smooth.spline(X1, Y_hat_med), col="blue")
+lines(smooth.spline(X1, Y_hat_lb), lty=2, col="red") #0.025
+lines(smooth.spline(X1, Y_hat_ub), lty=2, col="red") #0.975
 
 yrep=extract(sm)$Y_hat
 Y_hat_med <- array(NA, length(Y)) #median estimate
@@ -107,6 +99,7 @@ t3=reshape2::melt(t3, id=c("month"))
 plot(t3$value~t3$month, type="l", col="grey", ylim=c(0,1), xaxt="n")
 axis(1, c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"), at=c(1:12))
 points(PB_female_con$proportion~PB_female_con$month, col="blue", pch=16)
-lines(smooth.spline(X, Y_hat_med), col="red")
+lines(smooth.spline(X1, Y_hat_med), col="red")
 
-nd=lag(all_bmass3$ndvi, k=1)
+#model output summary####
+print(sm, pars=c("a0", "ndvi_eff"))
